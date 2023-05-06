15cfb09ac9a256beaf0ab36338ba766f6f8ee20a
Hans Felix <hansfelix50@gmail.com>
2023-05-06 00:38:33 -0500

    finish first logic

--- src/runtime/evaluation.ts
+++ src/runtime/evaluation.ts
@@ -3,5 +3,33 @@ import { ExpressionType } from "../lang/ExpressionTypes";
 
 // TODO: implement evaluate expression to accomplish tests
 export function evaluate(expression: Expression): any {
-  return new Error("not implemented yet");
+  let response : string | boolean | number = "";
+
+  switch (expression.type) {
+    case ExpressionType.If:
+      const ifFalse = expression.payload.if_false;
+      const ifTrue = expression.payload.if_true;
+      const testExpression = expression.payload.test_expression;
+
+      // Evaluate testExpression
+      const responseTestExpression = evaluate(testExpression);
+
+      // validate testExpression
+      if (responseTestExpression) {
+        response = evaluate(ifTrue);
+      } else {
+        response = evaluate(ifFalse);
+      }
+      break;
+
+    case ExpressionType.Const:
+      response = expression.payload.value;
+      break;
+
+    default:
+      break;
+  }
+
+  return response;
 }
+
